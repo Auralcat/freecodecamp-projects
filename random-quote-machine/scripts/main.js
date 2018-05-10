6021462c1e3712e55cb039b118e17fea0d079931
Auralcat <ashleyzt@hi2.in>
2018-05-10 20:25:02 -0300

    Add quote author and click event

--- random-quote-machine/scripts/main.js
+++ random-quote-machine/scripts/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    console.log("jQuery is working");
-    // Test API integration
-    $.getJSON('http://quotes.rest/qod.json?category=inspire', function(data) {
-        let quoteText = data.contents.quotes[0].quote;
-        let quoteAuthor = data.contents.quotes[0].author;
+    $("#anotherQuote").click(function() {
+        alert("You clicked the another quote button!");
+        $.getJSON('http://quotes.rest/qod.json?category=inspire', function(data) {
+            let quoteText = data.contents.quotes[0].quote;
+            let quoteAuthor = data.contents.quotes[0].author;
 
-        // Change jumbotron contents afterwards
+            // Change jumbotron contents afterwards
+        });
     });
-    $.ajax();
 });
