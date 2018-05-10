346cf3583f1a32476618f645c64ccbcda163ab98
Auralcat <ashleyzt@hi2.in>
2018-05-10 20:27:30 -0300

    Change page elements when grabbing quote from API

--- random-quote-machine/scripts/main.js
+++ random-quote-machine/scripts/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $("#anotherQuote").click(function() {
-        alert("You clicked the another quote button!");
         $.getJSON('http://quotes.rest/qod.json?category=inspire', function(data) {
             let quoteText = data.contents.quotes[0].quote;
             let quoteAuthor = data.contents.quotes[0].author;
 
             // Change jumbotron contents afterwards
+            $("#quote").text(quoteText);
+            $("#author").text(quoteAuthor);
         });
     });
 });
