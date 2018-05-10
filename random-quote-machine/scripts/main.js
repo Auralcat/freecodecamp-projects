225ff559d0d978c37b645d48ae81b12e578bb0ab
Auralcat <ashleyzt@hi2.in>
2018-05-10 20:23:02 -0300

    Grab quote text and author

--- random-quote-machine/scripts/main.js
+++ random-quote-machine/scripts/main.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
     console.log("jQuery is working");
     // Test API integration
     $.getJSON('http://quotes.rest/qod.json?category=inspire', function(data) {
-        console.log("AJAX worked");
-        console.log(data);
+        let quoteText = data.contents.quotes[0].quote;
+        let quoteAuthor = data.contents.quotes[0].author;
+
+        // Change jumbotron contents afterwards
     });
     $.ajax();
 });
