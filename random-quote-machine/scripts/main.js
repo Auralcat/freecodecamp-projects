9eb0dc4ea6109bb4d41bd9c9ff2b59e4f2d791f9
Auralcat <ashleyzt@hi2.in>
2018-05-10 19:09:41 -0300

    Sketch initial script

--- random-quote-machine/scripts/main.js
+++ random-quote-machine/scripts/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+    console.log("jQuery is working");
+    // Test API integration
+    $.getJSON('https://andruxnet-random-famous-quotes.p.mashape.com/', function(data) {
+        console.log("AJAX worked");
+        console.log(data);
+    });
 });
