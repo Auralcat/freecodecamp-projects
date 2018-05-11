0bdfebb73967ac0186e98086380c76e754bf73c7
Auralcat <ashleyzt@hi2.in>
2018-05-11 11:42:15 -0300

    Sketch display of weather status

--- show-local-weather/scripts/main.js
+++ show-local-weather/scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // First you need to get the user's lat and long
     navigator.geolocation.getCurrentPosition(function(pos) {
-        // Get weather through lat and long
+        // Get current latitude and longitude
         let latitude = pos.coords.latitude;
         let longitude = pos.coords.longitude;
 
@@ -12,9 +12,15 @@ $(document).ready(function() {
         let location = "?lat=" + latitude + "&lon=" + longitude;
 
         console.log(apiEndpoint + location);
+
+        // Get weather through lat and long
         $.getJSON(apiEndpoint + location, function(data) {
             console.log(data);
             console.log(data.weather[0].icon);
+
+            $("#currentTemperature").text("Current temperature: " + data.main.temp);
+            $("#minTemperature").text("Mininum temperature: " + data.main.temp_min);
+            $("#maxTemperature").text("Maximum temperature: " + data.main.temp_max);
         });
     });
 });
