ff65de45a8774f4e411c3e089ab768c18b76a4bf
Auralcat <ashleyzt@hi2.in>
2018-05-10 22:44:13 -0300

    Remove unused class, adjust typing time

--- random-quote-machine/scripts/main.js
+++ random-quote-machine/scripts/main.js
@@ -9,19 +9,27 @@ $(document).ready(function() {
             let quoteText = data.quote;
             let quoteAuthor = data.character;
 
+            // Get the wait time by dividing the quote's length by the typing
+            // speed * 0.3
+            let waitTime = Math.ceil(quoteText.length/0.024) + 300;
+            console.log(`${quoteText.length} / 0.015 = ${waitTime}`);
+
+
             // Type the content!
             let typedQuote = new Typed("#quote", {
                 strings: [quoteText],
                 typeSpeed: 30
             });
 
+            // Wait for the quote to finish, then write author's name
             setTimeout(function() {
                 let typedAuthor = new Typed("#author", {
                     strings: [quoteAuthor],
                     typeSpeed: 30
                 });
-            }, 5000);
-
+                // Remove typed cursor
+                $(".typed-cursor").hide();
+            }, waitTime);
         });
     });
 
