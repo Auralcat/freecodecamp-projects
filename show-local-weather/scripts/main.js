23fe3500592742bc050d73fe5d4edb2d8c336789
Auralcat <ashleyzt@hi2.in>
2018-05-11 11:16:06 -0300

    Touch weather API access script

--- show-local-weather/scripts/main.js
+++ show-local-weather/scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+    // First you need to get the user's lat and long
+    let userLatitude;
+    let userLongitude;
 
+    // Get weather through lat and long
+    $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139", function(data) {
+
+    });
 });
