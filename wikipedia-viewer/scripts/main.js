61e288854c355a667179cca01f44d8e0f74eacb4
Auralcat <ashleyzt@hi2.in>
2018-05-14 19:52:39 -0300

    Create algorithms to show search results

--- wikipedia-viewer/scripts/main.js
+++ wikipedia-viewer/scripts/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+    alert("jQuery is working");
     $("#searchBox").keyup(function() {
-        $.getJSON("", function(data) {
+        let searchTerm = $("#searchBox").val();
+        if (searchTerm != '') {
+            // It works!
+            // $("#searchResults").prepend("<p>" + searchTerm + "</p>");
+            // Make API request
+            $.getJSON("", function(data) {
+                $("#searchResults").html(data);
+            });
+        } else {
+            $("#searchResults").html('');
+        }
 
-        });
     });
 });
