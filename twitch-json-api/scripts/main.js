8be590b7cfc7343f7a3ba83dc2a183fd209369a5
Auralcat <ashleyzt@hi2.in>
2018-05-17 09:13:27 -0300

    Get streamer status, add names to table from list in JS

--- twitch-json-api/scripts/main.js
+++ twitch-json-api/scripts/main.js
@@ -2,13 +2,23 @@ let streamerList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
                     "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 $(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "https://wind-bow.gomix.me/twitch-api/streams",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function(data) {
-            console.log(data);
-        }
+    streamerList.forEach(function(streamerName) {
+        $.ajax({
+            type: "GET",
+            url: "https://wind-bow.gomix.me/twitch-api/streams/" + streamerName,
+            async: true,
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8",
+            // Use JSONP - JSON with padding - for cross origin requests.
+            // I'm not sure about how safe it is, but it works. :P
+            dataType: "jsonp",
+            success: function(data, status, xhr) {
+                $("#streamerTable").append(
+                    $("<tr />").append($("<td />").text(streamerName)));
+                if (Object.keys(data.stream).length != 0) {
+                    console.log(`${streamerName} is streaming right now.`);
+                }
+            }
+        });
     });
 });
