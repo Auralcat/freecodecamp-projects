3d58a07b78f90b43be5f8b316bb1afacec0eae30
Auralcat <ashleyzt@hi2.in>
2018-05-17 13:55:01 -0300

    Put stream details under username

--- twitch-json-api/scripts/main.js
+++ twitch-json-api/scripts/main.js
@@ -13,25 +13,30 @@ $(document).ready(function() {
             success: function(data, status, xhr) {
                 let $streamerPanel = $("<div />").addClass("streamer-panel");
                 let streamerLink = "https://twitch.tv/" + streamerName;
+                let $nameAndDetails = $("<div />").addClass("name-and-details");
 
                 // Add components of div
                 $streamerPanel.append($("<img />").attr("alt", "Sample image"));
-                $streamerPanel.append($("<h3 />").text(streamerName));
+
+                $nameAndDetails.append($("<h3 />").text(streamerName));
+                $streamerPanel.append($nameAndDetails);
 
                 // Add link to stream panel
                 let $wrappedStreamerPanel = $("<a />").attr("href", streamerLink)
                                             .addClass("channel-link");
                 $wrappedStreamerPanel.append($streamerPanel);
-                let $clone = $wrappedStreamerPanel.clone();
-                $("#allStreams").append($clone);
 
                 if (data.stream != null) {
                     console.log(`${streamerName} is streaming right now.`);
-                    $streamerPanel.append($("<h5/>").text(data.stream.channel.status));
+                    $nameAndDetails.append($("<h6/>").text(data.stream.channel.status));
                     $("#online").append($wrappedStreamerPanel);
                 } else {
                     $("#offline").append($wrappedStreamerPanel);
                 }
+
+                // Add to all streams
+                let $clone = $wrappedStreamerPanel.clone();
+                $("#allStreams").append($clone);
             }
         });
     });
