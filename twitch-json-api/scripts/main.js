802d87f7995b33047087694d05b5bdf44fb3d1cc
Auralcat <ashleyzt@hi2.in>
2018-05-17 14:42:50 -0300

    refactor ajax requests

--- twitch-json-api/scripts/main.js
+++ twitch-json-api/scripts/main.js
@@ -1,46 +1,60 @@
 let streamerList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
                     "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+let ajaxBaseObject = {
+    type: "GET",
+    crossDomain: true,
+    contentType: "application/json; charset=utf-8",
+    // Use JSONP - JSON with padding - for cross origin requests.
+    // I'm not sure about how safe it is, but it works. :P
+    dataType: "jsonp"
+};
+
 $(document).ready(function() {
+    let imageRequestObject = ajaxBaseObject;
+
     streamerList.forEach(function(streamerName) {
-        $.ajax({
-            type: "GET",
-            url: "https://wind-bow.gomix.me/twitch-api/streams/" + streamerName,
-            crossDomain: true,
-            contentType: "application/json; charset=utf-8",
-            // Use JSONP - JSON with padding - for cross origin requests.
-            // I'm not sure about how safe it is, but it works. :P
-            dataType: "jsonp",
-            success: function(data, status, xhr) {
-                let $streamerPanel = $("<div />").addClass("streamer-panel");
-                let streamerLink = "https://twitch.tv/" + streamerName;
-                let $nameAndDetails = $("<div />").addClass("name-and-details");
+        imageRequestObject.url = "https://wind-bow.gomix.me/twitch-api/users/" + streamerName;
+        imageRequestObject.success = function(data, status, xhr) {
+            let imageLink = data.logo;
+        };
 
-                // Add components of div
-                $streamerPanel.append($("<img />").attr("alt", "Sample image"));
+        // Request images
+        $.ajax(imageRequestObject);
 
-                $nameAndDetails.append($("<h3 />").text(streamerName));
-                $streamerPanel.append($nameAndDetails);
+        ajaxBaseObject.url = "https://wind-bow.gomix.me/twitch-api/streams/" + streamerName;
+        ajaxBaseObject.success = function(data, status, xhr) {
+            let $streamerPanel = $("<div />").addClass("streamer-panel");
+            let streamerLink = "https://twitch.tv/" + streamerName;
+            let $nameAndDetails = $("<div />").addClass("name-and-details");
 
-                // Add link to stream panel
-                let $wrappedStreamerPanel = $("<a />").attr("href", streamerLink)
-                                            .addClass("channel-link");
-                $wrappedStreamerPanel.append($streamerPanel);
+            // Add components of div
+            $streamerPanel.append($("<img />").attr("alt", "Pic link goes here"));
 
-                if (data.stream != null) {
-                    console.log(`${streamerName} is streaming right now.`);
-                    $nameAndDetails.append($("<h6/>").text(data.stream.channel.status));
-                    $streamerPanel.append($("<img />")
-                                          .addClass("status-icon")
-                                          .attr("src", "assets/img/check.svg"));
-                    $("#online").append($wrappedStreamerPanel);
-                } else {
-                    $("#offline").append($wrappedStreamerPanel);
-                }
+            $nameAndDetails.append($("<h3 />").text(streamerName));
+            $streamerPanel.append($nameAndDetails);
 
-                // Add to all streams
-                let $clone = $wrappedStreamerPanel.clone();
-                $("#allStreams").append($clone);
+            // Add link to stream panel
+            let $wrappedStreamerPanel = $("<a />").attr("href", streamerLink)
+                                        .addClass("channel-link");
+            $wrappedStreamerPanel.append($streamerPanel);
+
+            if (data.stream != null) {
+                console.log(`${streamerName} is streaming right now.`);
+                $nameAndDetails.append($("<h6/>").text(data.stream.channel.status));
+                $streamerPanel.append($("<img />")
+                                        .addClass("status-icon")
+                                        .attr("src", "assets/img/check.svg"));
+                $("#online").append($wrappedStreamerPanel);
+            } else {
+                $("#offline").append($wrappedStreamerPanel);
             }
-        });
+
+            // Add to all streams
+            let $clone = $wrappedStreamerPanel.clone();
+            $("#allStreams").append($clone);
+        };
+
+        // Request stream status
+        $.ajax(ajaxBaseObject);
     });
 });
