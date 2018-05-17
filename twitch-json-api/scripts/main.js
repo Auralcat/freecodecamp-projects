e2764a7b12efdf4fe4a299222210671fb11eedd0
Auralcat <ashleyzt@hi2.in>
2018-05-16 21:31:05 -0300

    Create AJAX request

--- twitch-json-api/scripts/main.js
+++ twitch-json-api/scripts/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-
+    $.ajax({
+        type: "GET",
+        url: "link",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+        }
+    });
 });
