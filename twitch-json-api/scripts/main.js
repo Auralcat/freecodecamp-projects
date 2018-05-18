784f36dc3089fa7aff18a05cc34998bd2b90719c
Auralcat <ashleyzt@hi2.in>
2018-05-18 18:03:57 -0300

    Remove contents from requestResources

--- twitch-json-api/scripts/main.js
+++ twitch-json-api/scripts/main.js
@@ -16,68 +16,56 @@ let ajaxBaseObject = {
     dataType: "jsonp"
 };
 
-let ajaxImgObj = ajaxBaseObject;
-let ajaxStatusObj = ajaxBaseObject;
+// Hard copy the objects!
+let ajaxImgObj = JSON.parse(JSON.stringify(ajaxBaseObject));
+ajaxImgObj.url = "https://wind-bow.gomix.me/twitch-api/users/"
+                        + streamerObj.name;
+ajaxImgObj.success = function(data, status, xhr) {
+    streamerObj.profilePic = data.logo;
+};
 
-function requestResources(ajaxBaseObject) {
-    // Fetches the images, links and stream status from the API.
-    streamerDataList.forEach(function(streamerObj) {
-        // Generate the link
-        streamerObj.profileLink = "https://twitch.tv/" + streamerObj.name;
-
-        ajaxImgObj.url = "https://wind-bow.gomix.me/twitch-api/users/"
-                             + streamerObj.name;
-        ajaxImgObj.success = function(data, status, xhr) {
-            streamerObj.profilePic = data.logo;
-        };
-
-        ajaxStatusObj.url = "https://wind-bow.gomix.me/twitch-api/streams/"
-                             + streamerObj.name;
-        ajaxStatusObj.success = function(data, status, xhr) {
-            if (data.stream != null) {
-                streamerObj.streamStatus = "online";
-            } else {
-                streamerObj.streamStatus = "offline";
-            }
-        };
-
-        // Request images
-        $.ajax(ajaxImgObj);
-
-        // Request stream status
-        $.ajax(ajaxStatusObj);
-    });
-}
+let ajaxStatusObj = JSON.parse(JSON.stringify(ajaxBaseObject));
+ajaxStatusObj.url = "https://wind-bow.gomix.me/twitch-api/streams/"
+                        + streamerObj.name;
+ajaxStatusObj.success = function(data, status, xhr) {
+    if (data.stream != null) {
+        streamerObj.streamStatus = "online";
+    } else {
+        streamerObj.streamStatus = "offline";
+    }
+};
 
 $(document).ready(function() {
-    $.when(requestResources(ajaxBaseObject)).done(function() {
-
+    $.when($.ajax(ajaxImgObj), $.ajax(ajaxStatusObj)).done(function() {
+        console.log(JSON.stringify(streamerDataList));
         streamerDataList.forEach(function(streamerObj) {
-            ajaxBaseObject.url = "https://wind-bow.gomix.me/twitch-api/streams/" + streamerObj.name;
-            ajaxBaseObject.success = function(data, status, xhr) {
-                let $streamerPanel = $("<div />").addClass("streamer-panel");
+            // Generate the link
+            streamerObj.profileLink = "https://twitch.tv/" + streamerObj.name;
+            // Create base divs
+            let $streamerPanel = $("<div />").addClass("streamer-panel");
+            let $nameAndDetails = $("<div />").addClass("name-and-details");
 
-                let $nameAndDetails = $("<div />").addClass("name-and-details");
+            // Add components of div
+            $streamerPanel.append($("<img />").attr("src", streamerObj.profilePic));
 
-                // Add components of div
-                $streamerPanel.append($("<img />").attr("src", streamerObj.profilePic));
+            $nameAndDetails.append($("<h3 />").text(streamerObj.name));
+            if (streamerObj.hasOwnProperty("streamStatus")) {
+                $nameAndDetails.append($("<h6 />").text(streamerObj.streamStatus));
+            }
+            $streamerPanel.append($nameAndDetails);
 
-                $nameAndDetails.append($("<h3 />").text(streamerObj.name));
-                $streamerPanel.append($nameAndDetails);
+            // Add link to stream panel
+            let $wrappedStreamerPanel = $("<a />").attr("href", streamerObj.profileLink)
+                .addClass("channel-link");
+            $wrappedStreamerPanel.append($streamerPanel);
 
-                // Add link to stream panel
-                let $wrappedStreamerPanel = $("<a />").attr("href", streamerObj.profileLink)
-                    .addClass("channel-link");
-                $wrappedStreamerPanel.append($streamerPanel);
+            // Add to div according to stream status
+            console.log(streamerObj);
+            $("#" + streamerObj.streamStatus).append($wrappedStreamerPanel);
 
-
-                // Add to all streams
-                let $clone = $wrappedStreamerPanel.clone();
-                $("#allStreams").append($clone);
-            };
-
-            // Request stream status
-            $.ajax(ajaxBaseObject);
+            // Add to all streams
+            let $clone = $wrappedStreamerPanel.clone();
+            $("#allStreams").append($clone);
         });
     });
 });
