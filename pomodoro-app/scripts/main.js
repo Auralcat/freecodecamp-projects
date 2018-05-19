b0b4868610cbe7be9c4f0488d97ca2a7998129f2
Auralcat <ashleyzt@hi2.in>
2018-05-19 18:50:12 -0300

    Add customization for short and long breaks

--- pomodoro-app/scripts/main.js
+++ pomodoro-app/scripts/main.js
@@ -1,5 +1,6 @@
 let defaultPomodoroTime = 25;
-let defaultBreakTime = 5;
+let defaultShortBreakTime = 5;
+let defaultLongBreakTime = 15;
 
 function showTime(rawTime) {
     // Receives raw time and returns the formatted string
@@ -22,6 +23,9 @@ function showTime(rawTime) {
 $(document).ready(function() {
     let countTime;
     let timer = defaultPomodoroTime * 60 * 1000;
+    let shortBreakTime = defaultShortBreakTime * 60 * 1000;
+    let longBreakTime = defaultLongBreakTime * 60 * 1000;
+
     $("#visor").text(showTime(timer));
 
     $("#startPomodoro").click(function() {
@@ -50,5 +54,15 @@ $(document).ready(function() {
         let inputLength = prompt("Type the desired pomodoro duration in minutes:");
         timer = inputLength * 60 * 1000;
         $("#visor").text(showTime(timer));
+    });
+
+    $("#customizeShortBreak").click(function() {
+        let inputShortBreak = prompt("Type the desired short break duration in minutes:");
+        shortBreakTime = inputShortBreak * 60 * 1000;
+    });
+
+    $("#customizeLongBreak").click(function() {
+        let inputLongBreak = prompt("Type the desired long break duration in minutes:");
+        longBreakTime = inputLongBreak * 60 * 1000;
     });
 });
