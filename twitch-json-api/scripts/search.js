9cac305b020b694234a8f61e1e4e23930ea3aa8f
Auralcat <ashleyzt@hi2.in>
2018-05-19 13:47:50 -0300

    Hide tabs when searching, show results correctly

--- twitch-json-api/scripts/search.js
+++ twitch-json-api/scripts/search.js
@@ -1,9 +1,23 @@
 // Implementing the search function.
 $(document).ready(function() {
-    $("#searchBox").keyup(function() {
+    $("#searchBox").keyup(function(e) {
+        // First, clear the results div and hide all tabs
+        e.preventDefault();
+        $(".tab-content").children().each(function() {
+            $(this).removeClass("active show");
+        });
+        $("#searchResults").remove();
+
         // Get contents from search box
         let searchTerm = $(this).val();
 
         // Find the streamers which match the query.
+        let result = $("#allStreams").find($("#" + searchTerm));
+
+        // Show the div
+        let $searchResults = $("<div />").attr("id", "searchResults")
+                .addClass("tab-pane fade in active show");
+        $searchResults.append(result);
+        $(".tab-content").append($searchResults);
     });
 });
