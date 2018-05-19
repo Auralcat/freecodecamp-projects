931d3acf35dbbc8b96f6fa2220246d257ee7fc4a
Auralcat <ashleyzt@hi2.in>
2018-05-19 18:20:49 -0300

    Change visor name, create showTime

--- pomodoro-app/scripts/main.js
+++ pomodoro-app/scripts/main.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+let defaultPomodoroTime = 25;
+let defaultBreakTime = 5;
 
+function showTime(visor) {
+    let minutes, seconds = visor.val().split(":");
+    if (seconds <= 0) {
+        seconds = 59;
+        minutes -= 1;
+    }
+    visor.val(`${minutes}:${seconds}`);
+}
+
+$(document).ready(function() {
+    $("#startPomodoro").click(function() {
+
+    });
 });
