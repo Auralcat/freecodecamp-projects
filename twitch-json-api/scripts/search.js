72d215080af6abfc0145dc75087bd874e0a2d6ca
Auralcat <ashleyzt@hi2.in>
2018-05-19 14:04:20 -0300

    Hide search results when empty

--- twitch-json-api/scripts/search.js
+++ twitch-json-api/scripts/search.js
@@ -15,15 +15,16 @@ $(document).ready(function() {
                 .addClass("tab-pane fade in active show");
 
         // Find the streamers which match the query.
-        $("#allStreams").children().each(function() {
-            let regex = new RegExp(searchTerm, "i");
-            console.log(regex.test($(this).attr("id")));
-            if (regex.test($(this).attr("id"))) {
-                // This is necessary so the panel doesn't vanish!
-                let result = $(this).clone();
-                $searchResults.append(result);
-            }
-        });
+        if (searchTerm !== "") {
+            $("#allStreams").children().each(function() {
+                let regex = new RegExp(searchTerm, "i");
+                if (regex.test($(this).attr("id"))) {
+                    // This is necessary so the panel doesn't vanish!
+                    let result = $(this).clone();
+                    $searchResults.append(result);
+                }
+            });
+        }
 
         // Show the div
         $(".tab-content").append($searchResults);
