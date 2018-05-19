ba808c877322858772218d500efded6d1c989046
Auralcat <ashleyzt@hi2.in>
2018-05-19 13:08:43 -0300

    Get the contents from the search box

--- twitch-json-api/scripts/search.js
+++ twitch-json-api/scripts/search.js
@@ -1,6 +1,9 @@
 // Implementing the search function.
 $(document).ready(function() {
-    $("#searchBox").onKeyUp(function() {
+    $("#searchBox").keyup(function() {
+        // Get contents from search box
+        let searchTerm = $(this).val();
 
+        // Find the streamers which match the query.
     });
 });
