1ac9489b0e674c38e911540f7cbe3954fa92eec5
Auralcat <ashleyzt@hi2.in>
2018-09-11 10:22:46 -0300

    Add general HTML

--- drum-machine/src/components/DrumPad.jsx
+++ drum-machine/src/components/DrumPad.jsx
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 export class DrumPad extends React.Component {
   render() {
     return(
-      <h1>This is a test with RJSX and Emmet</h1>
+    <div id="drum-machine" className="inner-container">
+    {/* <PadBank /> */}
+    <div className="logo">
+        <div className="inner-logo">{'Auralcat' + String.fromCharCode(160)}</div>
+    </div>
+    <div className="control">
+        <p>Power</p>
+        <div onClick="this.powerControl" className="select">
+            <div style="powerSlider" className="inner" />
+        </div>
+        <p id="display">{this.state.display}</p>
+        <div className="volume-slider">
+            <input max="1" min="0" name="volume" onChange={this.adjustVolume} step="0.01" type="range" value={this.state.sliderVal}/>
+        </div>
+    </div>
+    <div className="control">
+        <p>Bank</p>
+        <div className="select" onClick={ this.selectBank }>
+            <div style={ bankSlider } className="inner" />
+        </div>
+    </div>
+    </div>
     )
   }
 }
