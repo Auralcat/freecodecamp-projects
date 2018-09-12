d87bea53bbf28343db21262d627fc649e2e145c4
Auralcat <ashleyzt@hi2.in>
2018-09-12 20:23:23 -0300

    Remove useless constructor

--- drum-machine/src/components/Display.jsx
+++ drum-machine/src/components/Display.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 export class Display extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return (
             <div>
