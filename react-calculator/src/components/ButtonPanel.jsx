8280ba21b76c44618a76b440f44b7c47bfa74390
Auralcat <ashleyzt@hi2.in>
2018-09-13 13:39:07 -0300

    Create ButtonPanel component

--- react-calculator/src/components/ButtonPanel.jsx
+++ react-calculator/src/components/ButtonPanel.jsx
@@ -4,12 +4,14 @@ class ButtonPanel extends Component {
   render() {
       // Declare the Button component
       // You need a fn to translate the number to the string
-      const Button = ({label, callback}) => {
+      const CalcButton = ({label, callback}) => {
           return <button id={ label } onClick={ callback(label) }>{ label }</button>
       }
 
       return (
-      <input value={ this.props.display } readonly/>
+          <div className="button-panel">
+              { this.props.buttonArray.map((elem) => <CalcButton label={ elem.label } callback={ elem.callback }/>) }
+          </div>
     );
   }
 }
