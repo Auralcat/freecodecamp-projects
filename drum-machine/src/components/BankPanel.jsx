2ee5cf0a996e14746f860b03068c3baeac900608
Auralcat <ashleyzt@hi2.in>
2018-09-12 21:22:28 -0300

    Set up callback inside BankPanel

--- drum-machine/src/components/BankPanel.jsx
+++ drum-machine/src/components/BankPanel.jsx
@@ -3,17 +3,23 @@ import React from 'react';
 // This component is used only here.
 const BankSelector = ({bankName, callback}) => {
     return (
-        <button onClick={() => bankName}>{bankName}</button>
+        <button onClick={() => callback(bankName)}>{bankName}</button>
     )
 }
 
 export class BankPanel extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentBank: 'FooBar'
+        }
+    }
     render() {
         return (
             <div>
               <p>Bank</p>
               <div className="btn-group">
-                  <BankSelector bankName="Smooth Piano Kit"/>
+                  <BankSelector bankName="Smooth Piano Kit" callback={(bankName) => this.setState({currentBank: bankName})}/>
               </div>
             </div>
         )
