1a44f5119dd4fc0060546b06606bb6860f5465e1
Auralcat <ashleyzt@hi2.in>
2018-09-15 10:36:00 -0300

    Put number to the right in display

--- react-calculator/src/components/Visor.jsx
+++ react-calculator/src/components/Visor.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Visor extends Component {
   render() {
     return (
-      <input id="display" value={ this.props.display } readOnly={ true }/>
+        <input id="display" value={ this.props.display } style={ { textAlign: "right" } }readOnly={ true }/>
     );
   }
 }
